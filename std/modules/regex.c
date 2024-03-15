a904c26210310b50f25e33c30b4e790ce997a232
Blood Rogue <adnaptimsib333@gmail.com>
2024-03-15 10:25:30 +0530

    added regex functions

--- std/modules/regex.c
+++ std/modules/regex.c
@@ -6,33 +6,23 @@
 
 static ObjModule *_regex_module = NULL;
 
-static BuiltinResult _regex_match(int argc, Obj **argv, UNUSED(Obj *, caller)) {
-    CHECK_ARG_COUNT(2)
-    CHECK_ARG_TYPE(ObjString, STRING, 0)
-    CHECK_ARG_TYPE(ObjString, STRING, 1)
-
-    pcre2_code *re;
+static BuiltinResult regex(char *pattern, size_t pattern_size, char *subject, size_t subject_size) {
     PCRE2_SIZE erroffset;
     int errcode;
     uint8_t buffer[128];
 
-    int rc;
-
-    const char *pattern = argv_0->chars;
-    size_t pattern_size = argv_0->raw_length;
-
-    const char *subject = argv_1->chars;
-    size_t subject_size = argv_1->raw_length;
     uint32_t options = 0;
 
-    re = pcre2_compile((const uint8_t *)pattern, pattern_size, options, &errcode, &erroffset, NULL);
+    pcre2_code *re =
+        pcre2_compile((const uint8_t *)pattern, pattern_size, options, &errcode, &erroffset, NULL);
+
     if (re == NULL) {
         pcre2_get_error_message(errcode, buffer, 120);
         return ERR((char *)buffer);
     }
 
     pcre2_match_data *match_data = pcre2_match_data_create_from_pattern(re, NULL);
-    rc = pcre2_match(re, (uint8_t *)subject, subject_size, 0, options, match_data, NULL);
+    int rc = pcre2_match(re, (uint8_t *)subject, subject_size, 0, options, match_data, NULL);
 
     if (rc < 0) {
         switch (rc) {
@@ -61,13 +51,137 @@ static BuiltinResult _regex_match(int argc, Obj **argv, UNUSED(Obj *, caller)) {
     return OK(instance);
 }
 
+static BuiltinResult _regex_search(int argc, Obj **argv, UNUSED(Obj *, caller)) {
+    CHECK_ARG_COUNT(2)
+    CHECK_ARG_TYPE(ObjString, STRING, 0)
+    CHECK_ARG_TYPE(ObjString, STRING, 1)
+
+    return regex(argv_0->chars, argv_0->raw_length, argv_1->chars, argv_1->raw_length);
+}
+
+static BuiltinResult _regex_match(int argc, Obj **argv, UNUSED(Obj *, caller)) {
+    CHECK_ARG_COUNT(2)
+    CHECK_ARG_TYPE(ObjString, STRING, 0)
+    CHECK_ARG_TYPE(ObjString, STRING, 1)
+
+    char *pattern = malloc(argv_0->raw_length + 2);
+    snprintf(pattern, argv_0->raw_length + 2, "^%.*s", argv_0->raw_length, argv_0->chars);
+
+    return regex(pattern, argv_0->raw_length + 1, argv_1->chars, argv_1->raw_length);
+}
+
+static BuiltinResult _regex_fullmatch(int argc, Obj **argv, UNUSED(Obj *, caller)) {
+    CHECK_ARG_COUNT(2)
+    CHECK_ARG_TYPE(ObjString, STRING, 0)
+    CHECK_ARG_TYPE(ObjString, STRING, 1)
+
+    char *pattern = malloc(argv_0->raw_length + 3);
+    snprintf(pattern, argv_0->raw_length + 3, "^%.*s$", argv_0->raw_length, argv_0->chars);
+
+    return regex(pattern, argv_0->raw_length + 2, argv_1->chars, argv_1->raw_length);
+}
+
+static BuiltinResult _regex_findall(int argc, Obj **argv, UNUSED(Obj *, caller)) {
+    CHECK_ARG_COUNT(2)
+    CHECK_ARG_TYPE(ObjString, STRING, 0)
+    CHECK_ARG_TYPE(ObjString, STRING, 1)
+
+    const char *pattern = argv_0->chars;
+    size_t pattern_size = argv_0->raw_length;
+
+    const char *subject = argv_1->chars;
+    size_t subject_size = argv_1->raw_length;
+
+    PCRE2_SIZE erroffset;
+    int errcode;
+    uint8_t buffer[128];
+
+    uint32_t options = 0;
+
+    pcre2_code *re =
+        pcre2_compile((const uint8_t *)pattern, pattern_size, options, &errcode, &erroffset, NULL);
+
+    if (re == NULL) {
+        pcre2_get_error_message(errcode, buffer, 120);
+        return ERR((char *)buffer);
+    }
+
+    pcre2_match_data *match_data = pcre2_match_data_create_from_pattern(re, NULL);
+    int rc = pcre2_match(re, (uint8_t *)subject, subject_size, 0, options, match_data, NULL);
+
+    if (rc < 0) {
+        switch (rc) {
+            case PCRE2_ERROR_NOMATCH:
+                return OK(new_nil());
+            default:
+                return ERR("Matching error.");
+        }
+    }
+
+    ObjList *matches = new_list(NULL, 0);
+    ObjList *groups = new_list(NULL, 0);
+
+    PCRE2_SIZE *ovector = pcre2_get_ovector_pointer(match_data);
+    for (int i = 0; i < rc; i++) {
+        const char *start = subject + ovector[2 * i];
+        PCRE2_SIZE slen = ovector[2 * i + 1] - ovector[2 * i];
+        write_array(&groups->elems, AS_OBJ(new_string((char *)start, (int)slen)));
+    }
+
+    ObjInstance *instance = new_instance(get_regex_match_class());
+    SET_FIELD("groups", groups);
+
+    write_array(&matches->elems, AS_OBJ(instance));
+
+    bool cont = true;
+
+    while (cont) {
+        subject_size -= ovector[1];
+        subject += ovector[1];
+
+        rc = pcre2_match(re, (uint8_t *)subject, subject_size, 0, options, match_data, NULL);
+
+        if (rc < 0) {
+            switch (rc) {
+                case PCRE2_ERROR_NOMATCH:
+                    cont = false;
+                    break;
+                default:
+                    return ERR("Matching error.");
+            }
+        } else {
+            ObjList *groups = new_list(NULL, 0);
+
+            ovector = pcre2_get_ovector_pointer(match_data);
+            for (int i = 0; i < rc; i++) {
+                const char *start = subject + ovector[2 * i];
+                PCRE2_SIZE slen = ovector[2 * i + 1] - ovector[2 * i];
+                write_array(&groups->elems, AS_OBJ(new_string((char *)start, (int)slen)));
+            }
+
+            ObjInstance *instance = new_instance(get_regex_match_class());
+            SET_FIELD("groups", groups);
+
+            write_array(&matches->elems, AS_OBJ(instance));
+        }
+    }
+
+    pcre2_match_data_free(match_data);
+    pcre2_code_free(re);
+
+    return OK(matches);
+}
+
 ObjModule *get_regex_module(int count, UNUSED(char **, parts)) {
     CHECK_PART_COUNT(0)
 
     if (_regex_module == NULL) {
         ObjModule *module = new_module(new_string("regex", 5));
 
+        SET_BUILTIN_FN_MEMBER("search", _regex_search);
         SET_BUILTIN_FN_MEMBER("match", _regex_match);
+        SET_BUILTIN_FN_MEMBER("fullmatch", _regex_fullmatch);
+        SET_BUILTIN_FN_MEMBER("findall", _regex_findall);
 
         _regex_module = module;
     }
